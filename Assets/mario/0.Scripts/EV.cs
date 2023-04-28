2bf1d1cb4ac162c227dbddf9880de3943f4d9685
mebsi <dbsrjs1224@gmail.com>
2023-04-28 21:53:43 +0900

    4/28 EV

--- Assets/mario/0.Scripts/EV.cs
+++ Assets/mario/0.Scripts/EV.cs
@@ -14,64 +14,127 @@ public class EV : MonoBehaviour
     public Door fDoor;
     public Door bDoor;
 
-    
+    [SerializeField] List<Transform> floors;
+
+    int currentFloor = 0;   //현재 층
+
+    bool isUp = true;
+
+    float doorSpeed = 0.1f;
     void Start()
     {
-        StartCoroutine(DoorAnimation());
+        //Invoke("Move", 2f);
+        Move();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F1))
+        { 
+            StartCoroutine("MainEV");
+        }      
+    }
+    void Move()
+    {
+        StartCoroutine(MainEV());
+    }
+
+    IEnumerator MainEV()
+    {
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine("DoorAnimation");
+        yield return StartCoroutine("CMove");
     }
     IEnumerator DoorAnimation()
     {
         //왼쪽은 Z 값을 더해준다.
         //초기값은 0.25 맥스 값은 0.84
-        yield return new WaitForSeconds(3f);
-
-        while (true)
-        {
-            if(fDoor.left.localPosition.x >= 0.75)
-            {
-                Vector3 lpos = fDoor.left.position;
-                lpos.x = 0.75f;
-                fDoor.left.position = lpos;
-
-                Vector3 rpos = fDoor.right.position;
-                rpos.x = -0.75f;
-                fDoor.right.position = rpos;
-
-                break;
-            }
-            fDoor.left.Translate(new Vector3(0.01f, 0, 0));
-            fDoor.right.Translate(new Vector3(-0.01f, 0, 0));
-
-            yield return new WaitForSeconds(Time.deltaTime * 10f);
-        }
-
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1.5f);
 
         while (true)    //열림
         {
+            if(fDoor.left.localPosition.x >= 0.84)
+            {
+                Vector3 lpos = fDoor.left.localPosition;
+                lpos.x = 0.84f;
+                fDoor.left.localPosition = lpos;
+
+                Vector3 rpos = fDoor.right.localPosition;
+                rpos.x = -0.84f;
+                fDoor.right.localPosition = rpos;
+
+                break;
+            }
+            fDoor.left.Translate(new Vector3(0.01f, 0f, 0f));
+            fDoor.right.Translate(new Vector3(-0.01f, 0f, 0f));
+
+            yield return new WaitForSeconds(Time.deltaTime * doorSpeed);    //delay Time
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        while (true)    //닫힘
+        {
             if (fDoor.left.localPosition.x <= 0.25)
             {
-                Vector3 lpos = fDoor.left.position;
+                Vector3 lpos = fDoor.left.localPosition;
                 lpos.x = 0.25f;
-                fDoor.left.position = lpos;
+                fDoor.left.localPosition = lpos;
 
-                Vector3 rpos = fDoor.right.position;
+                Vector3 rpos = fDoor.right.localPosition;
                 rpos.x = -0.25f;
-                fDoor.right.position = rpos;
+                fDoor.right.localPosition = rpos;
 
                 break;
             }
             fDoor.left.Translate(new Vector3(-0.01f, 0, 0));
             fDoor.right.Translate(new Vector3(0.01f, 0, 0));
 
-            yield return new WaitForSeconds(Time.deltaTime * 10f);
+            yield return new WaitForSeconds(Time.deltaTime * doorSpeed);    //delay Time
         }
 
         yield return null;
+    }
+
+    IEnumerator CMove() //층 수 이동
+    {
+        while (true)
+        {
+            if (isUp)   //6층에 도달하면 1층으로 내려오기
+            {
+                if (transform.localPosition.y >= floors[currentFloor].localPosition.y)
+                {
+                    /*
+                    Vector3 vec = transform.localPosition;
+                    vec.y = floors[currentFloor].localPosition.y;
+                    transform.localPosition = vec;
+                    */
+                    break;
+                }
+                transform.Translate(Vector3.up * Time.deltaTime * 30f);
+            }
+            else
+            {
+
+            }
+            transform.Translate(Vector3.up * Time.deltaTime * 30f);
+
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        yield return new WaitForSeconds(0.5f);  //delay Time
+        if (isUp)   //6층에 도달하면 1층으로 내려오기
+        {                        
+            isUp = false;
+            currentFloor--;
+        }            
+        else
+        {
+            currentFloor--;
+            if (currentFloor <= 0)
+                isUp = true;
+        }
+        StartCoroutine("CMove");
     }
 }
--- Assets/mario/0.Scripts/FollowCam.cs
+++ Assets/mario/0.Scripts/FollowCam.cs
@@ -20,7 +20,7 @@ public class FollowCam : MonoBehaviour
             return;
         }
         Vector3 pos = target.position;
-        pos.z -= 5f;
+        pos.z -= 3f;
         pos.y += 1f;
         transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 7f);
     }
